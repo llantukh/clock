6ab83276d9c115b02bcea0242c9b229b3bbc8667
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-10-11 18:13:02 -0400

    initial commit...I forgot to commit earlier

---
+++ main.js
@@ -0,0 +1,32 @@
+var clock = function (){
+  var time = new Date ();
+
+  var hour = time.getHours();
+  var minute = time.getMinutes();
+  var second = time.getSeconds();
+
+  var showHour = document.getElementById("hours");
+  if (hour >= 10) {
+    showHour.innerHTML = hour + ":"
+  } else {
+    showHour.innerHTML = 0 + hour +":"
+  }
+
+  var showMinute = document.getElementById("minutes");
+  if (minute >= 10) {
+    showMinute.innerHTML = minute + ":"
+  } else {
+    showMinute.innerHTML = 0 + minute +":"
+  }
+
+  var showSecond = document.getElementById("seconds");
+  if (second >= 10) {
+    showSecond.innerHTML = second
+  } else {
+    showSecond.innerHTML = 0 + second
+  }
+};
+
+clock();
+
+setInterval (clock, 1000);
