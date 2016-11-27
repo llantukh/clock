88c59b4292ff4dae3af2495be34c0b1d684b3127
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-11-27 18:17:35 -0500

    made corrections according to feedback

--- main.js
+++ main.js
@@ -26,37 +26,11 @@ var clock = function (){
   } else {
     showSecond.innerHTML = "0" + second
   }
-};
 
-setInterval (clock, 1000);
-
-//getting the hex digits to change
-var color = function (){
-var time = new Date ();
-
-var hour = time.getHours();
-var minute = time.getMinutes();
-var second = time.getSeconds();
-
-  // var showHex = document.getElementById("hexColor");
-  // showHex.innerHTML = "<p>#" + hour + minute + second + "</p>"
-};
-setInterval (color, 1000);
-
-
-// getting the background color to change
-var hexyNumber = 123456;
-
-var hexyChange = function (){
-  hexyNumber = hexyNumber + 10;
-}
-
-var hexyBackground = function (){
   var hexyColor = document.getElementById("hexy");
-  hexyColor.innerHTML = "<div style = \"background-color:" + hexyNumber + ";\"> <p style=\"color:" + (1000000-hexyNumber) + ";\"> #" + hexyNumber + "</p></div>";
-  hexyColor.style.backgroundColor = ("#" + hexyNumber);
+    hexyColor.innerHTML = "#" + hour + minute + second
+    hexyColor.style.backgroundColor = "#" + hour + minute + second
+
 };
 
 setInterval (clock, 1000);
-setInterval (hexyBackground, 500);
-setInterval (hexyChange, 500);
